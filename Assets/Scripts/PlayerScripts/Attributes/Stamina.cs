c5908b901d0c2fe9d54cc92c63bb16ccbe055cf8
Radoslav Chobanov <rado.chobanov97@gmail.com>
2021-09-10 17:34:33 +0300

    Stamina logic

--- Assets/Scripts/PlayerScripts/Attributes/Stamina.cs
+++ Assets/Scripts/PlayerScripts/Attributes/Stamina.cs
@@ -1,18 +1,76 @@
 using System;
+using UnityEngine;
 
 [Serializable]
 public class Stamina : Attribute
 {
-    private const float defaultMinStamina = 0;
-    private const float defaultMaxStamina = 10;
-    private const float defaultStaminaRegen = 5;
+    private float degenerateSpeed; // stanima/second (stamina per second) decreasing when running
+    private float degenerateValue; // how much stamina it will burn per tick
+    private float lastDegenerateTime;
+
+    private float regenerateSpeed;
+    private float lastRegenerateTime;
 
     public Stamina()
     {
-        this.min = defaultMinStamina;
-        this.max = defaultMaxStamina;
-        this.regen = defaultStaminaRegen;
+        this.min = Values.defaultMinStamina;
+        this.max = Values.defaultMaxStamina;
+        this.regen = Values.defaultStaminaRegen;
+
+        this.degenerateSpeed = Values.defaultStaminaDegenerateSpeed;
+        this.degenerateValue = Values.defaultStaminaDegenerateValue;
+
+        this.regenerateSpeed = Values.defaultStaminaRegenerateSpeed;
 
         this.current = this.max;
+    }
+    private void Degenerate()
+    {
+        if (current - degenerateValue < min)
+            current = min;
+        else current -= degenerateValue;
+
+        lastDegenerateTime = Time.time;
+    }
+    public void StartDegenerate()
+    {
+        lastDegenerateTime = Time.time;
+    }
+    private bool ShouldDegenerate()
+    {
+        return Time.time - lastDegenerateTime >= degenerateSpeed;
+    }
+    public void UpdateDegenerate()
+    {
+        if (ShouldDegenerate())
+        {
+            Degenerate();
+        }
+    }
+
+    private void Regenerate()
+    {
+        if (current + regen > max)
+            current = max;
+        else current += regen * regenerateSpeed;
+
+        lastRegenerateTime = Time.time;
+    }
+    public void StartRegenerate()
+    {
+        lastRegenerateTime = Time.time;
+    }
+    private bool ShouldRegenerate()
+    {
+        if (current >= max)
+            return false;
+        return Time.time - lastRegenerateTime >= regenerateSpeed; // per second
+    }
+    public void UpdateRegenerate()
+    {
+        if (ShouldRegenerate())
+        {
+            Regenerate();
+        }
     }
 }
---
+++ Assets/Scripts/UtilityScripts/Values.cs
@@ -0,0 +1,13 @@
+using System;
+
+public static class Values
+{
+    #region Stamina
+    public const float defaultStaminaRegen = 3f;
+    public const float defaultMinStamina = 0f;
+    public const float defaultMaxStamina = 10f;
+    public const float defaultStaminaRegenerateSpeed = 0.2f;
+    public const float defaultStaminaDegenerateSpeed = 1f; // stanima/second (stamina per second) decreasing when running
+    public const float defaultStaminaDegenerateValue = 1f; // how much stamina it will burn per tick
+    #endregion
+}
